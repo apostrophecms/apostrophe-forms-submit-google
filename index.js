22e15ec3d658b8fb9448cb2f92b347d6780fbc54
Alex Bea <alex.bea@gmail.com>
2019-06-25 14:47:19 -0500

    Controls sheet name field with boolean

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
         {
           label: 'Yes',
           value: true,
-          showFields: [ 'googleSpreadsheetId' ]
+          showFields: [ 'googleSpreadsheetId', 'googleSheetName' ]
         }
       ]
     },
