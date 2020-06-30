607ad0c0aaf26b47b6bf9e435b899f2097b520e5
Tom Boutell <tom@punkave.com>
2020-06-30 12:25:58 -0400

    all within if statement

--- index.js
+++ index.js
@@ -45,16 +45,15 @@ module.exports = {
 
     self.sendToGoogle = async function (req, form, data) {
 
-      // Don't modify the original, this would frustrate
-      // other custom submission handlers
-      data = Object.assign({}, data);
-      const timeFields = (new Date()).toISOString().match(/^(.*?)T(.*?)(\..*)$/);
-      data['Date Submitted'] = timeFields[1];
-      data['Time Submitted'] = timeFields[2];
-
-      await self.emit('beforeSubmit', req, form, data);
-
       if (form.googleSheetSubmissions === true) {
+        // Don't modify the original, this would frustrate
+        // other custom submission handlers
+        data = Object.assign({}, data);
+        const timeFields = (new Date()).toISOString().match(/^(.*?)T(.*?)(\..*)$/);
+        data['Date Submitted'] = timeFields[1];
+        data['Time Submitted'] = timeFields[2];
+
+        await self.emit('beforeSubmit', req, form, data);
         if (!form.googleSheetName) {
           try {
             form.googleSheetName = await getFirstSheet(form.googleSpreadsheetId);
